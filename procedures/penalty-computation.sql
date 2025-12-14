ecda8c348d9f6a4a1b92478fa23ba7783d6854c4
Keneth Rivera <riverakenethjames@gmail.com>
2025-12-14 10:07:21 +0800

    Computation for Penalty Procedures
    compute penalties for overdues, damaged, and lost.

--- procedures/penalty-computation.sql
+++ procedures/penalty-computation.sql
@@ -1,10 +1,83 @@
 -- Stored Procedure: Penalty Computation
--- Author: [Your Name]
+-- Author: [Keneth James E. Rivera]
 -- Description: Calculate and insert penalties for overdue/lost books
 
 USE LibrarySystem;
 
-DELIMITER //
+-- Compute Penalties (STORED PROCEDURES)
+BEGIN	
+    DECLARE var_dueDate DATE;
+    DECLARE var_returnedDate DATE;
+    DECLARE var_status VARCHAR(20);
+    DECLARE var_daysOverdue INT DEFAULT 0;
+    DECLARE var_penaltyAmount DECIMAL(10,2) DEFAULT 0;
+    DECLARE var_reason VARCHAR(20) DEFAULT NULL;
+    
+    -- Since borrow log ID is a parameter, this is will hande missing and not correct log ID
+    IF NOT EXISTS (
+        SELECT 1 FROM borrowlogs WHERE BorrowLogID = p_BorrowLogID
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'BorrowLogID not found';
+    END IF;
+    
+    SELECT b.ReturnDate, b.DueDate, b.Status
+    INTO var_returnedDate, var_dueDate, var_status
+    FROM borrowlogs b
+    WHERE BorrowLogID = p_BorrowLogID;
+    
+    IF var_returnedDate IS NOT NULL AND var_returnedDate > var_dueDate THEN
+        SET var_daysOverdue = DATEDIFF(var_returnedDate, var_dueDate);
+        SET var_penaltyAmount = var_penaltyAmount + (var_daysOverdue * 100);
+        SET var_reason = CONCAT_WS(', ', var_reason, 'Overdue');
+    END IF;
+    
+    IF var_status = 'LOST'
+    THEN 
+    	SET var_penaltyAmount = var_penaltyAmount + 5000;
+        SET var_reason = CONCAT_WS(', ', var_reason, 'LOST');
+    END IF;
+    
+    IF var_status = 'Damaged' 
+    THEN
+        SET var_penaltyAmount = var_penaltyAmount + 1000;
+        SET var_reason = CONCAT_WS(', ', var_reason, 'Damaged');  
+    END IF;
+    
+    IF var_penaltyAmount > 0 THEN
+        INSERT INTO penalty (
+            BorrowLogID,
+            DaysOverdue,
+            PenaltyAmount,
+            PenaltyReason
+        )
+        VALUES (
+            p_BorrowLogID,
+            var_daysOverdue,
+            var_penaltyAmount,
+            TRIM(var_reason)
+        );
+    END IF;
+    
+    SELECT 
+        log.BorrowLogID,
+        u.FullName,
+        b.BookTitle,
+        NULLIF(TRIM(v.PenaltyReason), '') AS PenaltyType,
+        v.DaysOverdue,
+        v.TotalPenalty
+    FROM borrowlogs log 
+    LEFT JOIN users u ON log.UserID = u.UserID
+    LEFT JOIN books b ON log.BookID = b.BookID
+    JOIN (
+    	SELECT 
+            var_daysOverdue AS DaysOverdue,
+            var_penaltyAmount AS TotalPenalty,
+            var_reason AS PenaltyReason
+	) AS v
+    WHERE log.BorrowLogID = p_BorrowLogID;
+
+END
 
 -- TODO: Create procedure for penalty computation
 -- CREATE PROCEDURE CalculatePenalty(IN borrowLogID INT)
@@ -17,7 +90,7 @@ DELIMITER //
 --     -- Update penalty table
 -- END //
 
-DELIMITER ;
+
 
 -- Test the procedure
 -- CALL CalculatePenalty(1);
